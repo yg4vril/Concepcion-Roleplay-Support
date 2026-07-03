f89f91b482a3d245e34751d37d03b8374768736d
yg4vril <yg4vril@gmail.com>
2026-07-03 11:08:11 -0400

    Change bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "LaMafia Rusa Dominando",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 5,
       },
     ],
   },
