a50dc4db01e5f1b02973d3aa1b7b4806bdd23e26
yg4vril <yg4vril@gmail.com>
2026-07-03 11:40:45 -0400

    Update support.js

--- src/commands/Core/support.js
+++ src/commands/Core/support.js
@@ -3,7 +3,7 @@ import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
-const SUPPORT_SERVER_URL = "https://discord.gg/QnWNz2dKCE";
+const SUPPORT_SERVER_URL = "https://discord.gg/5Zwex6DfEu";
 export default {
     data: new SlashCommandBuilder()
     .setName("support")
