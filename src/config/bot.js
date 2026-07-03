21a5a006e089ffac0bc233a106762aa3c15bd838
yg4vril <yg4vril@gmail.com>
2026-07-03 11:22:54 -0400

    Update bot activity name with emoji

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "La Mafia Rusa Dominando",
+        name: "🇷🇺 La Mafia Rusa Dominando 🇷🇺",
         // Activity type number (0 = Playing).
         type: 5,
       },
